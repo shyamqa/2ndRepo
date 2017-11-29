df5eb6b66e585252892dfaa5d0ba45734fb36ce6
shyamqa <dsprasad.qa@gmail.com>
2017-11-29 16:36:40 +0530

    while loop is added

--- GitHubDemoApps/src/com/demo/java/GitHubDemoApps.java
+++ GitHubDemoApps/src/com/demo/java/GitHubDemoApps.java
@@ -3,9 +3,12 @@ package com.demo.java;
 public class GitHubDemoApps {
 
 	public static void main(String[] args) {
-		
+		int i=0;
+			while(i<=4) {
 			System.out.println("this is empty now");
+			i+=1;
 	}
 	
+	}
 	
 }
