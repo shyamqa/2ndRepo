d64e0afce596c3e4429eeeea2456116a9c92ea1a
shyamqa <dsprasad.qa@gmail.com>
2017-11-29 17:11:10 +0530

    while loop is removed and for loop is added

--- GitHubDemoApps/src/com/demo/java/GitHubDemoApps.java
+++ GitHubDemoApps/src/com/demo/java/GitHubDemoApps.java
@@ -4,11 +4,8 @@ public class GitHubDemoApps {
 
 	public static void main(String[] args) {
 		int i=0;
-			while(i<=4) {
-			System.out.println("this is empty now");
-			i+=1;
-	}
-	
+		for(;i<=10;i++)
+			System.out.println("this is no longer empty now");
 	}
 	
 }
