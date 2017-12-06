5e6b941306af2a9bd211198a33a98c6fb837e25e
shyamqa <dsprasad.qa@gmail.com>
2017-12-06 19:39:40 +0530

    Update GitHubDemoApps.java
    take an updations

--- GitHubDemoApps/src/com/demo/java/GitHubDemoApps.java
+++ GitHubDemoApps/src/com/demo/java/GitHubDemoApps.java
@@ -5,7 +5,7 @@ public class GitHubDemoApps {
 	public static void main(String[] args) {
 		int i=0;
 		for(;i<=10;i++)
-			System.out.println("this is no longer empty now");
+			System.out.println("This is no longer empty now");
 	}
 	
 }
